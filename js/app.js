00a03e77d15478ae4a68d7c5713cd0bd7dcea940
DASyhef <daspoubelle@hotmail.fr>
2023-05-04 15:15:31 +0200

    square' s here

--- js/app.js
+++ js/app.js
@@ -19,6 +19,7 @@ document.getElementById("btnOne").addEventListener('click', function() {
     document.querySelector(".orange").setAttribute('class', 'figure orange square');
 });
 
+
 document.getElementById("btnTwo").addEventListener('click', function() {
     document.querySelector(".red").setAttribute('class', 'figure red ');
     document.querySelector(".green").setAttribute('class', 'figure green ');
@@ -70,11 +71,11 @@ document.getElementById("btnSix").addEventListener('click', function() {
 });
 
 document.getElementById("btnSeven").addEventListener('click', function() {
-    document.querySelector(".red").setAttribute('class', 'figure red ');
-    document.querySelector(".green").setAttribute('class', 'figure green ');
-    document.querySelector(".blue").setAttribute('class', 'figure blue ');
-    document.querySelector(".yellow").setAttribute('class', 'figure yellow ');
-    document.querySelector(".purple").setAttribute('class', 'figure purple ');
-    document.querySelector(".brown").setAttribute('class', 'figure brown ');
-    document.querySelector(".orange").setAttribute('class', 'figure orange ');
+    document.querySelector(".red").setAttribute('class', 'figure red boat');
+    document.querySelector(".green").setAttribute('class', 'figure green boat');
+    document.querySelector(".blue").setAttribute('class', 'figure blue boat');
+    document.querySelector(".yellow").setAttribute('class', 'figure yellow boat');
+    document.querySelector(".purple").setAttribute('class', 'figure purple boat');
+    document.querySelector(".brown").setAttribute('class', 'figure brown boat');
+    document.querySelector(".orange").setAttribute('class', 'figure orange boat');
 });
